5665d269cf0675e0d9695496efa93e44fdc55a7a
rtetelbaum <rtetelbaum@mailbox.org>
2021-01-11 14:03:15 -0500

    added nav buttons, ffinished footer, basic wireframe done

--- src/App.js
+++ src/App.js
@@ -6,31 +6,34 @@ function App() {
 
 			<div id="nav">
 				<div id="logo">
-					<h1>Logo</h1>
+					<img src="/logo200.png" alt="logo"/>
 				</div>
-				<h1>nav div</h1>
-				<h1>nav div</h1>
-				<h1>nav div</h1>
+				<button>Home</button>
+				<button>Sign Up</button>
+				<button>Log In</button>
+				<button>Saved Quizzes</button>
+				<button>Create Quiz</button>
 			</div>
 
 			<div id="main">
-				<h1>main div first h1</h1>
-				<h1>main div</h1>
-				<h1>main div</h1>
-				<h1>main div</h1>
-				<h1>main div</h1>
-				<h1>main div</h1>
-				<h1>main div</h1>
-				<h1>main div</h1>
-				<h1>main div</h1>
-				<h1>main div</h1>
-				<h1>main div</h1>
-				<h1>main div</h1>
-				<h1>main div last h1</h1>
+				<h1>Home Component</h1>
+				<h1>Sign Up Component</h1>
+				<h1>Log In Component</h1>
+				<h1>Quiz Container</h1>
+				<h1>Create Quiz Component</h1>
+				<h1>Quiz Component</h1>
+				<h1>Email Quiz Component</h1>
+				<h1>Submit Quiz Component</h1>
+				<h1>Delete Quiz Component</h1>
+				<h1>Create Question Component</h1>
+				<h1>Question Component</h1>
+				<h1>Edit Question Component</h1>
+				<h1>Delete Question Component</h1>
 			</div>
 
 			<div id="footer">
-				<h3>footer</h3>
+				<p>Copyright © Quizler {new Date().getFullYear()} A Roman Tetelbaum Project&nbsp;</p>
+				<a href="https://github.com/rtetelbaum/quizler-frontend" target="_blank" rel="noreferrer"><img src="/github.png" alt="github" /></a>
 			</div>
 
 		</div>
