8b34704c11ba86e7508ab136bfb5d70b941ca0db
rtetelbaum <rtetelbaum@mailbox.org>
2021-01-20 22:51:01 -0500

    nav buttons css cleanup

--- src/Components/NavComponent.js
+++ src/Components/NavComponent.js
@@ -13,68 +13,64 @@ function NavComponent(props) {
 				<Link to="/home"><img src="/logo200x100.png" alt="logo" /></Link>
 			</div>
 
-			<div id="nav-button-div">
+			<Link to="/home"><Button type="button" label="Home" icon="pi pi-home" className="p-button-raised p-button-rounded" /></Link>
 
-				<Link to="/home"><Button type="button" label="Home" icon="pi pi-home" className="p-button-raised p-button-rounded" /></Link>
-
-				{
-					props.user
+			{
+				props.user
+					?
+					props.user.id
 						?
-						props.user.id
-							?
-							null
-							:
-							<Link to="/signup"><Button type="button" label="Sign Up" icon="pi pi-user-plus" className="p-button-raised p-button-rounded" /></Link>
+						null
 						:
 						<Link to="/signup"><Button type="button" label="Sign Up" icon="pi pi-user-plus" className="p-button-raised p-button-rounded" /></Link>
-				}
+					:
+					<Link to="/signup"><Button type="button" label="Sign Up" icon="pi pi-user-plus" className="p-button-raised p-button-rounded" /></Link>
+			}
 
-				{
-					props.user
+			{
+				props.user
+					?
+					props.user.id
 						?
-						props.user.id
-							?
-							null
-							:
-							<Link to="/login"><Button type="button" label="Log In" icon="pi pi-sign-in" className="p-button-raised p-button-rounded" /></Link>
+						null
 						:
 						<Link to="/login"><Button type="button" label="Log In" icon="pi pi-sign-in" className="p-button-raised p-button-rounded" /></Link>
-				}
+					:
+					<Link to="/login"><Button type="button" label="Log In" icon="pi pi-sign-in" className="p-button-raised p-button-rounded" /></Link>
+			}
 
-				{
-					props.user
+			{
+				props.user
+					?
+					props.user.id
 						?
-						props.user.id
-							?
-							<Link to="/home"><Button type="button" onClick={() => props.logOutUser()} label="Log Out" icon="pi pi-sign-out" className="p-button-raised p-button-rounded" /></Link>
-							: null
+						<Link to="/home"><Button type="button" onClick={() => props.logOutUser()} label="Log Out" icon="pi pi-sign-out" className="p-button-raised p-button-rounded" /></Link>
+						: null
+					:
+					null
+			}
+
+			{
+				props.user
+					? props.user.id
+						? <Link to="/quizzes"><Button type="button" label="Saved Quizzes" icon="pi pi-save" className="p-button-raised p-button-rounded" /></Link>
 						:
 						null
-				}
+					:
+					null
+			}
 
-				{
-					props.user
-						? props.user.id
-							? <Link to="/quizzes"><Button type="button" label="Saved Quizzes" icon="pi pi-save" className="p-button-raised p-button-rounded" /></Link>
-							:
-							null
-						:
-						null
-				}
-
-				{
-					props.user
+			{
+				props.user
+					?
+					props.user.id
 						?
-						props.user.id
-							?
-							<Link to="/quizzes/create"><Button type="button" label="Create Quiz" icon="pi pi-pencil" className="p-button-raised p-button-rounded" /></Link>
-							:
-							null
+						<Link to="/quizzes/create"><Button type="button" label="Create Quiz" icon="pi pi-pencil" className="p-button-raised p-button-rounded" /></Link>
 						:
 						null
-				}
-
-			</div>
+					:
+					null
+			}
 
 		</div>
 	)
