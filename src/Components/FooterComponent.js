764c104d29be0a81427fe85ccf30bc9b8bdc4794
rtetelbaum <rtetelbaum@mailbox.org>
2021-01-22 00:02:50 -0500

    added linkedin and medium logo links in footer

--- src/Components/FooterComponent.js
+++ src/Components/FooterComponent.js
@@ -6,6 +6,8 @@ function FooterComponent() {
 			<p className="p-component">Copyright © Quizler {new Date().getFullYear()} A Roman Tetelbaum Project&nbsp;&nbsp;</p>
 
 			<a href="https://github.com/rtetelbaum/quizler-frontend" target="_blank" rel="noreferrer"><img src="/GitHub-Mark-Light-32px.png" alt="github" /></a>
+			<a href="https://www.linkedin.com/in/rtetelbaum/" target="_blank" rel="noreferrer"><img className="footer-logo" src="/LinkedIn.png" alt="linkedin" /></a>
+			<a href="https://rtetelbaum.medium.com/" target="_blank" rel="noreferrer"><img className="footer-logo" src="/Medium.png" alt="linkedin" /></a>
 		</div>
 	)
 }
