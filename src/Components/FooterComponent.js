f03e6dbe76ac54a355085a6adb034d968dfa0066
rtetelbaum <rtetelbaum@mailbox.org>
2021-02-24 12:06:20 -0500

    updated footer

--- src/Components/FooterComponent.js
+++ src/Components/FooterComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 function FooterComponent() {
 	return (
 		<div id="footer-div">
-			<p className="p-component">Copyright © Quizler {new Date().getFullYear()} A Roman Tetelbaum Project&nbsp;&nbsp;</p>
+			<p className="p-component">Copyright © Quizler {new Date().getFullYear()} - A Roman Tetelbaum Project&nbsp;&nbsp;</p>
 
 			<a href="https://github.com/rtetelbaum/quizler-frontend" target="_blank" rel="noreferrer"><img src="/GitHub.png" alt="github" /></a>
 			<a href="https://www.linkedin.com/in/rtetelbaum/" target="_blank" rel="noreferrer"><img className="footer-logo" src="/LinkedIn.png" alt="linkedin" /></a>
